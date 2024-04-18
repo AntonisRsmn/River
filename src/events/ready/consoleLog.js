75b0b537b61ca0909f9d904cef9e0d9d762531dc
Antonis Rusman <antonisxgr05@gail.com>
2024-04-18 11:19:34 +0300

    Update 18/4/2024 11:19

--- src/events/ready/consoleLog.js
+++ src/events/ready/consoleLog.js
@@ -1,18 +1,31 @@
 const { client, ActivityType } = require("discord.js");
+const mongoose = require("mongoose");
+const mongoURL = process.env.MONGO_URL;
 require("colors");
 
-module.exports = (client) => {
-    console.log(`[INFO] ${client.user.username} Is online`.white);
+module.exports = async (client) => {
+  console.log(`[INFO] ${client.user.username} Is online`.white);
 
-    setInterval(() => {
-        const act = [
-            { name: `@${client.user.username}`, type: ActivityType.Listening },
-            { name: `${client.guilds.cache.size} Servers`, type: ActivityType.Watching },
-        ];
+  setInterval(() => {
+    const act = [
+      { name: `@${client.user.username}`, type: ActivityType.Listening },
+      {
+        name: `${client.guilds.cache.size} Servers`,
+        type: ActivityType.Watching,
+      },
+    ];
 
-        var random = act[Math.floor(Math.random() * act.length)];
-        client.user.setPresence({
-            activities: [random]
-        })
-    }, 1000 * 10);
+    var random = act[Math.floor(Math.random() * act.length)];
+    client.user.setPresence({
+      activities: [random],
+    });
+  }, 1000 * 10);
+
+  if (!mongoURL) return;
+  mongoose.set("strictQuery", true);
+
+  await mongoose.connect(mongoURL);
+  if (mongoose.connect) {
+    console.log(`[INFO] Connected to the database!`.green);
+  }
 };
