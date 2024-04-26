2ba12af194bd74e29afe3b2fcea1e06fec526ec5
Antonis Rusman <antonisxgr05@gail.com>
2024-04-26 03:02:50 +0300

    Update 26/4/2024 03:03

---
+++ src/commands/Fun/meme.js
@@ -0,0 +1,30 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const fetch= (...args) => import("node-fetch").then(({ default: fetch}) => fetch(...args));
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("meme")
+    .setDescription("Sends some funny memes."),
+
+  run: async(client, interaction) => {
+    await fetch("https://www.reddit.com/r/shitposting/random/.json").then(async res => {
+            let meme = await res.json();
+
+            let title = meme[0].data.children[0].data.title;
+            let url = meme[0].data.children[0].data.url;
+            let author = meme[0].data.children[0].data.author;
+
+            const embed = new EmbedBuilder()
+            .setTitle(`${title}`)
+            .setImage(`${url}`)
+            .setURL(url)
+            .setColor("fffffe")
+            .setFooter({
+                text: `By ${interaction.user.username}`,
+                iconURL: interaction.user.displayAvatarURL(),
+              })
+
+            return interaction.reply({ embeds: [embed] });
+        })
+  },
+};
