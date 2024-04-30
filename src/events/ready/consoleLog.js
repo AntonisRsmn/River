9aa5fdbf7043eedd430b301909c5368d755f4423
Antonis Rusman <antonisxgr05@gail.com>
2024-04-30 14:32:42 +0300

    Update 30/4/2024 14:33 Version: 1.2.3

--- src/events/ready/consoleLog.js
+++ src/events/ready/consoleLog.js
@@ -1,8 +1,16 @@
 const { client, ActivityType } = require("discord.js");
+const mongoose = require("mongoose");
+const mongoURL = process.env.MONGO_URL;
 require("colors");
 
 module.exports = async (client) => {
   console.log(`[INFO] ${client.user.username} Is online`.white);
+
+  if (!mongoURL) return;
+  mongoose.set("strictQuery", true);
+  if (await mongoose.connect(mongoURL)) {
+    console.log(`[INFO] Connected to the database`.green);
+  }
 
   setInterval(() => {
     const act = [
